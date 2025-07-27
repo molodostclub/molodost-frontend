996807d373c0287df6d0bc1d1b371a6fe2617b5b
Lev Shushkov <martynova@mauri-group.ru>
2025-07-27 08:19:55 +0300

    Update contacts.utils.ts

--- src/utils/contacts.utils.ts
+++ src/utils/contacts.utils.ts
@@ -1,4 +1,4 @@
 export const trimTel = (tel: string) => tel.replace(/\s/g, '');
 export const telHref = (tel: string) => `tel:${tel}`;
 export const whatsappHref = (tel: string) => `https://wa.me/${tel}`;
-export const telegramHref = 'https://t.me/ClubMolodost';
+export const telegramHref = 'https://t.me/molodostturclub';
