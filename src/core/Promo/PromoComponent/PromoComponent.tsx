a4c590239a8f77b3bc5ed95a009ec6b2662dd976
Lev Shushkov <martynova@mauri-group.ru>
2025-09-05 17:08:17 +0300

    Update PromoComponent.tsx

--- src/core/Promo/PromoComponent/PromoComponent.tsx
+++ src/core/Promo/PromoComponent/PromoComponent.tsx
@@ -13,12 +13,6 @@ import { Description, SectionHeading } from '@/uikit';
 type InputEvent = ChangeEvent<HTMLInputElement>;
 
 export function PromoComponent() {
-	const policyLink = () => (
-		<Link target="_blank" href="/docs/promo/policy.doc" className={styles.agreementLink}>
-			политикой конфиденциальности
-		</Link>
-	);
-
 	const agreementLink = () => (
 		<Link target="_blank" href="/docs/promo/personalAgreement.doc" className={styles.agreementLink}>
 			обработку моих персональных данных
@@ -31,18 +25,9 @@ export function PromoComponent() {
 		</Link>
 	);
 
-	const policyAgreementInit: Checkbox[] = [
-		{
-			label: 'Я соглашаюсь с',
-			linkText: policyLink(),
-			value: '5',
-			checked: false,
-		},
-	];
-
 	const personalAgreementInit: Checkbox[] = [
 		{
-			label: 'Я даю согласие на',
+			label: 'Даю свое согласие на',
 			linkText: agreementLink(),
 			value: '6',
 			checked: false,
@@ -51,7 +36,7 @@ export function PromoComponent() {
 
 	const marketingAgreementInit: Checkbox[] = [
 		{
-			label: 'Я даю согласие на',
+			label: 'Даю свое согласие на',
 			linkText: marketingLink(),
 			value: '7',
 			checked: false,
@@ -62,7 +47,6 @@ export function PromoComponent() {
 	const [surname, setSurname] = useState<string>('');
 	const [whatsapp, setWhatsapp] = useState<string>('');
 	const [attachment, setAttachment] = useState<File | null>(null);
-	const [policyAgreement, setPolicyAgreement] = useState<Checkbox[]>(policyAgreementInit);
 	const [personalAgreement, setPersonalAgreement] = useState<Checkbox[]>(personalAgreementInit);
 	const [marketingAgreement, setMarketingAgreement] = useState<Checkbox[]>(marketingAgreementInit);
 
@@ -119,23 +103,26 @@ export function PromoComponent() {
 				<>
 					<h1 className={cn(indent.mb_4, styles.pageTitle)}>АНКЕТА</h1>
 					<p className={styles.descriptionCenter} style={{ textTransform: 'none', fontWeight: 400 }}>
-						Заполните анкету, чтобы у&nbsp;нас остались ваши данные и&nbsp;мы&nbsp;могли прислать вам подарочный билет на&nbsp;бесплатное проживание в&nbsp;&laquo;Молодости&raquo; на&nbsp;Алтае!
+						Заполните анкету, чтобы у&nbsp;нас остались ваши данные и&nbsp;мы&nbsp;могли прислать вам билет на&nbsp;бесплатное проживание в&nbsp;&laquo;Молодости&raquo; на&nbsp;Алтае!
 					</p>
 					<br />
 					<form onSubmit={onSubmit}>
 						<div className={indent.mt_4}>
 							<div className={indent.mt_4}>
 								<Label caption="Имя">
+									<br />
 									<BaseInput value={name} required={false} onChange={(e: InputEvent) => setName(e.target.value)} />
 								</Label>
 							</div>
 							<div className={indent.mt_4}>
 								<Label caption="Фамилия">
+									<br />
 									<BaseInput value={surname} required={false} onChange={(e: InputEvent) => setSurname(e.target.value)} />
 								</Label>
 							</div>
 							<div className={indent.mt_4}>
 								<Label caption="Контактный номер WhatsApp">
+									<br />
 									<BaseInput type="text" placeholder="+7 999 999 99 99" value={whatsapp} regExp={/[^+\s\d]/gi} required={false} onChange={(e: InputEvent) => setWhatsapp(e.target.value)} />
 								</Label>
 							</div>
@@ -155,10 +142,6 @@ export function PromoComponent() {
 								</Label>
 							</div>
 						</div>
-
-						<div className={indent.mt_4}>
-							<BaseCheckbox name="policyAgreement" item={policyAgreement[0]} required={true} onChange={() => onChangeCheckbox(policyAgreement[0].value, policyAgreement, (data) => setPolicyAgreement(data))} />
-						</div>
 						<div className={indent.mt_4}>
 							<BaseCheckbox name="personalAgreement" item={personalAgreement[0]} required={true} onChange={() => onChangeCheckbox(personalAgreement[0].value, personalAgreement, (data) => setPersonalAgreement(data))} />
 						</div>
@@ -169,6 +152,15 @@ export function PromoComponent() {
 						<button type="submit" className={cn(styles.ctaBtn, indent.mt_4)} id="sendForm">
 							ОСТАВИТЬ ЗАЯВКУ
 						</button>
+						<div className={indent.mt_4}>
+							<small className={styles.noteBlack}>
+								Нажимая кнопку &laquo;Отправить&raquo;, я&nbsp;подтверждаю, что ознакомлен с&nbsp;
+								<Link target="_blank" href="/docs/promo/policy.doc" className={styles.agreementLink}>
+									Политикой обработки персональных данных
+								</Link>{' '}
+								Общества с&nbsp;ограниченной ответственностью &laquo;Молодость&raquo;
+							</small>
+						</div>
 					</form>
 				</>
 			)}
