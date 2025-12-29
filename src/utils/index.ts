b1ef1137b4feb9728ff174d26068187f9ebb34e6
EternumArt <martynova@mauri-group.ru>
2025-12-29 20:02:56 +0300

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,10 +2,5 @@ export { formatPrice, formatPriceWithSign } from './formatPrice';
 export * from './contacts.utils';
 export * from './backend';
 export { FormState } from './FormState';
-// Node.js модули экспортируем условно только для серверного использования
-// Они не должны попадать в клиентский бандл
-if (typeof window === 'undefined') {
-  // Эти модули используются только на сервере
-  // Экспортируем только типы, не функции
-  export type { ImageValidationResult, ImageValidationOptions } from './imageValidation';
-}
+// Экспортируем типы из imageValidation (не функции, только типы - безопасно для клиентского бандла)
+export type { ImageValidationResult, ImageValidationOptions } from './imageValidation';
