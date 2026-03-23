d65d9ce792420af712195274c09d45e63e9b795d
Lev Shushkov <martynova@mauri-group.ru>
2026-03-24 00:18:35 +0300

    fixes

--- src/pages/manjerok.tsx
+++ src/pages/manjerok.tsx
@@ -5,14 +5,14 @@ import { GetStaticProps } from 'next';
 
 type Props = HousesSplit & { trips: TripsSplit };
 
-export default function ManjerokPage({ individual, inHouse, trips }: Props) {
+export default function ManjerokPage({ individual, inHouse, luxiping, trips }: Props) {
 	return (
 		<>
 			<PageMeta title="Молодость в Манжероке" />
 			<InnerPageHeader />
 			<PageCover src="/images/main-cover2.jpg" />
 			<Breadcrumb items={[{ label: 'Главная', href: '/' }, { label: 'Экосистема Алтай', href: '/nasha-ekosistema' }, { label: 'Молодость в Манжероке' }]} />
-			<Manjerok houses={{ individual, inHouse }} trips={trips} />
+			<Manjerok houses={{ individual, inHouse, luxiping }} trips={trips} />
 			<Footer />
 		</>
 	);
